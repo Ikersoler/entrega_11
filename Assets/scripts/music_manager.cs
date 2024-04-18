318ea336670bf02e35882eed6ec2acea795b621c
Ikersoler <ikersonic@gmail.com>
2024-04-18 15:44:21 +0200

    falta programar

--- Assets/scripts/music_manager.cs
+++ Assets/scripts/music_manager.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class music_manager : MonoBehaviour
 {
+    [SerializeField] private AudioClip[] musicArray;
+
+    [SerializeField] private Sprite[] musicImages;
+
+    [SerializeField] private Button Play;
+    [SerializeField] private Button Pause;
+    [SerializeField] private Button Loop;
+    [SerializeField] private Button Shufle;
+    [SerializeField] private Button Next;
+    [SerializeField] private Button Previous;
+    [SerializeField] private Button Stop;
+    [SerializeField] private Slider SliderMusic;
+
+    [SerializeField] private Image imagePlace;
+
     
+
+    
+
+
+
+
+
 }
